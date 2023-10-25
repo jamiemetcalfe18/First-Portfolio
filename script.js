2dfd3b3f9aa95552f8a7c87405582642d02023ab
jamiemetcalfe18 <jamie.jet18@gmail.com>
2023-10-25 06:17:09 +0000

    Added skills section, favicons, made styling consistent, fixed top title and back to top button.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //to top btn
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() { scrollFunction() };
 
 function scrollFunction() {
   let mybutton = document.getElementById("myBtn");
@@ -16,15 +16,15 @@ function topFunction() {
 }
 
 // header scrolling effect
-$(window).on('scroll', function(){
-	if($(window).scrollTop()){
-      $('header').addClass('nav-show');
-		  
-	} 
-	else{
-		$('header').removeClass('nav-show');
-	}
-	   
+$(window).on('scroll', function() {
+  if ($(window).scrollTop()) {
+    $('header').addClass('nav-show');
+
+  }
+  else {
+    $('header').removeClass('nav-show');
+  }
+
 })
 
 // When the user clicks on the button, scroll to the top of the document
@@ -35,26 +35,26 @@ function topFunction() {
 
 //hamburger
 const navSlide = () => {
-	 const hamburger = document.querySelector(".hamburger");
-	 const navbar = document.querySelector(".nav-bar");
-	 const navLinks = document.querySelectorAll(".nav-bar li");
+  const hamburger = document.querySelector(".hamburger");
+  const navbar = document.querySelector(".nav-bar");
+  const navLinks = document.querySelectorAll(".nav-bar li");
 
-     hamburger.onclick = () => {
-		
-	 navbar.classList.toggle("nav-active");
-		 
-      //Animation links
-	 navLinks.forEach((link, index) => {
-		if (link.style.animation) {
-			link.style.animation = "";
-		} else {
-			link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7+1}s`;
-		   }
-		});
-	  //hamburger animation
-	 hamburger.classList.toggle("toggle");
-    }
-	 
-	}
+  hamburger.onclick = () => {
+
+    navbar.classList.toggle("nav-active");
+
+    //Animation links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1}s`;
+      }
+    });
+    //hamburger animation
+    hamburger.classList.toggle("toggle");
+  }
+
+}
 
 window.onload = () => navSlide();
